88efd630d76ef107b0dd60fd93e94c6ad6b37cc7
Peter Czigany <p.czigany@gmail.com>
2024-05-25 18:26:37 +0200

    add unit test scenario

---
+++ test/test_result.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from result import Result
+
+
+class TestResult(TestCase):
+
+    @patch('attempts.Attempts')
+    @patch('secret_number.SecretNumber')
+    def test_say_when_losing(self, mock_attempts, mock_secret_number):
+        # arrange
+        mock_attempts_instance = mock_attempts.return_value
+        mock_attempts_instance.matches.return_value = False
+        mock_secret_number_instance = mock_secret_number.return_value
+        mock_secret_number_instance.number.return_value = 42
+
+        # act
+        Result(mock_attempts_instance, mock_secret_number_instance).say()
+
+        # assert
+        mock_attempts_instance.matches.assert_called_once()
+        mock_secret_number_instance.number.assert_called_once()
