c16d1247c1cc5f8b7dfa732ca7d7f16ea23a9759
Peter Czigany <p.czigany@gmail.com>
2024-05-25 18:28:58 +0200

    add unit test scenario

--- test/test_result.py
+++ test/test_result.py
@@ -21,3 +21,19 @@ class TestResult(TestCase):
         # assert
         mock_attempts_instance.matches.assert_called_once()
         mock_secret_number_instance.number.assert_called_once()
+
+    @patch('attempts.Attempts')
+    @patch('secret_number.SecretNumber')
+    def test_say_when_winning(self, mock_attempts, mock_secret_number):
+        # arrange
+        mock_attempts_instance = mock_attempts.return_value
+        mock_attempts_instance.matches.return_value = True
+        mock_secret_number_instance = mock_secret_number.return_value
+        mock_secret_number_instance.number.return_value = 42
+
+        # act
+        Result(mock_attempts_instance, mock_secret_number_instance).say()
+
+        # assert
+        mock_attempts_instance.matches.assert_called_once()
+        mock_secret_number_instance.number.assert_not_called()
